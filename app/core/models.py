86f58e619ebf16b2110f831917df31245fb412c9
omaria@mobiticket.co.ke <omaria@mobiticket.co.ke>
2019-12-23 11:31:33 +0300

    Custom user model created

--- app/core/models.py
+++ app/core/models.py
@@ -8,7 +8,7 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, name, password=None, **extra_fields):
         """Create new user using email, name"""
-        user = self.model(email=email, **extra_fields)
+        user= self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
@@ -18,12 +18,12 @@ class User(AbstractBaseUser,PermissionsMixin):
 
     """Customizes user model that supports use of
     email to create user and not username"""
-    email = models.EmailField(max_length = 255, unique=True)
-    name = models.CharField(max_length = 255)
-    is_active = models.BooleanField(default = True)
-    is_staff = models.BooleanField(default = False)
+    email= models.EmailField(max_length = 255, unique=True)
+    name= models.CharField(max_length = 255)
+    is_active= models.BooleanField(default = True)
+    is_staff= models.BooleanField(default = False)
 
-    objects = UserManager()
+    objects= UserManager()
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name', ]
+    USERNAME_FIELD= 'email'
+    REQUIRED_FIELDS= ['name', ]
