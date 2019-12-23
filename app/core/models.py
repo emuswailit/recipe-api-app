1e748f2234e958ecb71edd1b5220ebd5519deedf
omaria@mobiticket.co.ke <omaria@mobiticket.co.ke>
2019-12-23 11:43:26 +0300

    Custom user model created

--- app/core/models.py
+++ app/core/models.py
@@ -3,10 +3,9 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
                                         PermissionsMixin
 
 
-
 class UserManager(BaseUserManager):
 
-    def create_user(self, email, name, password=None, **extra_fields):
+    def create_user(self, email, name, password= None, **extra_fields):
         """Create new user using email, name"""
         user= self.model(email=email, **extra_fields)
         user.set_password(password)
@@ -15,6 +14,7 @@ class UserManager(BaseUserManager):
         return user
 
 class User(AbstractBaseUser, PermissionsMixin):
+
 
     """Customizes user model that supports use of
     email to create user and not username"""
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,18 +1,19 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests (TestCase):
     """Class for defining model test cases"""
 
     def test_create_user_with_email_succesful(self):
         """Test if user with given email was created"""
-        email= 'test@test.com'
-        password= 'Pass123'
-        name= 'Micahel'
+        email = 'test@test.com'
+        password = 'Pass123'
+        name = 'Micahel'
 
         user = get_user_model().objects.create_user(
-            email= email,
-            name= name,
+            email = email,
+            name = name,
             password= password
         )
 
