844d2cc720f4ced74e69514f27703b0b9a4c5b70
omaria@mobiticket.co.ke <omaria@mobiticket.co.ke>
2019-12-23 11:17:27 +0300

    Custom user model created

---
+++ app/core/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser,BaseUserManager,PermissionsMixin
+
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self,email,name,password=None, **extra_fields):
+        """Create new user using email, name"""
+        user =self.model(email=email,**extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+class User(AbstractBaseUser,PermissionsMixin):
+    """Customizes user model that supports use of email to create user and not username"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active= models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name',]
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,21 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests (TestCase):
+    """Class for defining model test cases"""
+
+    def test_create_user_with_email_succesful(self):
+        """Test if user with given email was created"""
+        email = 'test@test.com'
+        password = 'Pass123'
+        name='Micahel'
+
+        user = get_user_model().objects.create_user(
+            email=email,
+            name=name,
+            password=password
+        )
+
+        self.assertEqual(user.email,email)
+        self.assertTrue(user.check_password(password))
