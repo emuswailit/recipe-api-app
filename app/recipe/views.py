7ecde64d679cd7fd7d9c593cc9a4fd069a96ca78
omaria@mobiticket.co.ke <omaria@mobiticket.co.ke>
2019-12-26 10:18:37 +0300

    Added image fields to recipe model

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -48,5 +48,8 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Return appropriate serializer class"""
         if self.action == 'retrieve':
             return serializers.RecipeDetailSerializer
-
         return self.serializer_class
+
+    def perform_create(self, serializer):
+        """Create a recipe object"""
+        serializer.save(user=self.request.user)
